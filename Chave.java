8755339ea924c34196afd0516b513fb23378628b
Reinaldo <reinaldosmoraes@hotmail.com>
2016-06-22 01:09:09 -0300

    Add files via upload

---
+++ Chave.java
@@ -0,0 +1,28 @@
+package ed2.aa.struct;
+
+
+public class Chave {
+
+	private int valor;
+	private Chave proxima;
+	
+	public Chave(int valor){
+		
+		this.valor = valor;
+	}
+	
+	public int exibirValor(){
+		
+		return this.valor;
+	}
+	
+	public void proximaChave(int valor){
+		
+		this.proxima = new Chave(valor);
+	}
+	
+	public Chave consultaProxima(){
+		
+		return this.proxima;
+	}
+}
